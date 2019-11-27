6277d2e0b9f0850aa037e8be2eb70b3bd447688a
Elias Norrby <elias.norrby@gmail.com>
2019-11-27 14:27:14 +0100

    Add env var to avoid pipe deprecation guard
    Adds and env var that, if set to true, disables the deprecation guard
    for pipe syntax.
    
    When running an ansible playbook, the test [ -p /dev/stdin ] will always
    be true. Thus, every 'zplug user/repo' will fail with a warning, even if
    the (deprecated) pipe syntax was not used.
    
    Another option would be to log the warning but add the plugin anyway,
    i.e. remove the return statement.
    
    Fixes: zplug/zplug#393, zplug/zplug#272

--- base/core/add.zsh
+++ base/core/add.zsh
@@ -6,7 +6,7 @@ __zplug::core::add::to_zplugs()
     local -a re_tags
 
     # DEPRECATED: pipe
-    if [[ -p /dev/stdin ]]; then
+    if [[ ! $ZPLUG_PIPE_FIX="true" && -p /dev/stdin ]]; then
         __zplug::core::migration::pipe
         return $status
     fi
